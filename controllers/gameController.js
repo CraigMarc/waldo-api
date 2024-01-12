36c84f8a3d763276f3c83212afa72312ef683b7e
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2024-01-12 12:55:28 -0500

    added route to check coordinates

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -4,15 +4,21 @@ const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 
 
-// get all published posts
+// check coords
 exports.check_get = asyncHandler(async (req, res, next) => {
-/*
+
   try {
-    let allPosts = await Posts.find({ 'published': true }).exec()
-    res.status(200).json(allPosts)
+    let character = await Game.findOne({ 'char_name': req.query.char_name }).exec()
+    
+   if ((req.query.x > character.max_x && req.query.x < character.min_x) || (req.query.y > character.max_y && req.query.y < character.min_y) ) {
+    res.json({message: true})
+   }
+   else {
+    res.json({message: false})
+   }
   } catch (error) {
     res.status(500).json({ message: error });
-  }*/
-  res.send('respond with a resource');
+  }
+  
 
 });
