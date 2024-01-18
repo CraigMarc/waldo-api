c40469b55339ca14927cc6c3882c328f95e54051
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2024-01-18 13:22:19 -0500

    sanitized post route

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -87,7 +87,7 @@ exports.end_time = asyncHandler(async (req, res, next) => {
 
 })
 
-
+/*
 exports.new_high_score = asyncHandler(async (req, res, next) => {
 
   try {
@@ -127,5 +127,65 @@ exports.new_high_score = asyncHandler(async (req, res, next) => {
   }
 
 
-})
+})*/
+
+exports.new_high_score = [
+
+  body("name").trim().escape(),
+ 
+
+  async function (req, res, next) {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.json({
+        data: req.body,
+        errors: errors.array(),
+      });
+      return;
+    }
+
+
+    try {
+
+      const [newScore, oldScore] = await Promise.all([
+        CurrentScore.findOne({ 'user_id': req.body.id }).exec(),
+        HighScore.find().sort({ score: -1 }).limit(1)
+      ]);
+      
+  
+      const highScore = new HighScore({
+        userName: req.body.name,
+        score: newScore.score,
+  
+      });
+  
+      if (oldScore.length == 0) {
+        await highScore.save()
+        await CurrentScore.findOneAndDelete({ 'user_id': req.body.id }).exec()
+  
+        return res.status(200).json({ message: "new score saved" })
+      }
+  
+      if (req.body.name == oldScore[0].userName) {
+        return res.status(200).json({ message: "username taken" })
+      }
+  
+      await highScore.save()
+      await HighScore.findOneAndDelete({ 'userName': oldScore[0].userName }).exec()
+      await CurrentScore.findOneAndDelete({ 'user_id': req.body.id }).exec()
+  
+      return res.status(200).json({ message: "new score saved" })
+    }
+  
+    catch (error) {
+      res.status(500).json({ message: error });
+    }
+  
+  
+  }
+
+
+  
+]
 
